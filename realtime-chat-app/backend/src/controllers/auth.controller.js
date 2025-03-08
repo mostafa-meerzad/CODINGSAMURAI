da4f7e554b4e504491cf202716ccf10e74761232
mostafa <mostafameerzad@gmail.com>
2025-03-08 21:28:34 +0430

    realtime-chat-app/backend: check if the user is authenticated

--- realtime-chat-app/backend/src/controllers/auth.controller.js
+++ realtime-chat-app/backend/src/controllers/auth.controller.js
@@ -102,3 +102,12 @@ export const updateProfile = async (req, ers) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const checkAuth = (req, res) => {
+  try {
+    return res.status(200).json(req.user);
+  } catch (error) {
+    console.log("error in check auth controller: ", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
--- realtime-chat-app/backend/src/routes/auth.route.js
+++ realtime-chat-app/backend/src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
+import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middlewares/auth-middleware.js";
 
 const router = Router();
@@ -8,5 +8,5 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 router.put("/update-profile", protectRoute, updateProfile)
-
+router.get("/check", protectRoute, checkAuth)
 export default router;
