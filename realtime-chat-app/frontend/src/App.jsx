d7979fe34b323dcae62449b47b8cf5fe82378dc7
mostafa <mostafameerzad@gmail.com>
2025-03-09 20:16:40 +0430

    realtime-chat-app/frontend: add basic routes

--- realtime-chat-app/frontend/src/App.jsx
+++ realtime-chat-app/frontend/src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
