c26726394891c3d05cd99d74cca2ef04a87a13a3
mostafa <mostafameerzad@gmail.com>
2025-03-09 14:22:46 +0430

    realtime-chat-app/backend: set up messages route

--- realtime-chat-app/backend/src/index.js
+++ realtime-chat-app/backend/src/index.js
@@ -1,21 +1,23 @@
-import express from "express"
-import authRoutes from "./routes/auth.route.js"
-import dotenv from "dotenv"
-import { connectDB } from "./lib/db.js"
-import cookieParser from "cookie-parser"
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import messageRoute from "./routes/message.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
-dotenv.config()
-const app = express()
+dotenv.config();
+const app = express();
 
-app.use(express.json())
-app.use(cookieParser())
-app.use("/api/auth", authRoutes)
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoute);
 
-app.get("/", (req, res)=>{
-    res.send("home page")
-})
-const PORT = process.env.PORT || 3000
-app.listen(PORT, ()=>{
-    console.log(`Server is running or port: ${PORT}`)
-    connectDB()
-})
+app.get("/", (req, res) => {
+  res.send("home page");
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running or port: ${PORT}`);
+  connectDB();
+});
