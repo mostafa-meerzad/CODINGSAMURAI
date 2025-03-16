24d3e8d22205974dcceba84eb146ef26c7818d6f
mostafa <mostafameerzad@gmail.cocm>
2025-03-16 10:58:07 +0430

    realtime-chat-app/frontend: add message createdAt time for better UX

--- realtime-chat-app/frontend/src/components/ChatContainer.jsx
+++ realtime-chat-app/frontend/src/components/ChatContainer.jsx
@@ -5,7 +5,7 @@ import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
 import MessageSkeleton from "./skeletons/MessageSkeleton";
 import { useAuthStore } from "../store/useAuthStore";
-// import { formatMessageTime } from "../lib/utils";
+import { formatMessageTime } from "../lib/utils";
 
 const ChatContainer = () => {
   const {
@@ -75,7 +75,7 @@ const ChatContainer = () => {
             </div>
             <div className="chat-header mb-1">
               <time className="text-xs opacity-50 ml-1">
-                {/* {formatMessageTime(message.createdAt)} */}
+                {formatMessageTime(message.createdAt)}
               </time>
             </div>
             <div className="chat-bubble flex flex-col">
