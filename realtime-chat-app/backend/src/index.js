ad4489a80f7287d5cc531d517e4508177c4ceded
mostafa <mostafameerzad@gmail.cocm>
2025-03-17 14:56:16 +0430

    realtime-chat-app/backend: remove redundant endpoint

--- realtime-chat-app/backend/src/index.js
+++ realtime-chat-app/backend/src/index.js
@@ -5,8 +5,8 @@ import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import {app, io, server} from "./lib/socket.js"
-import path from "path"
+import { app, io, server } from "./lib/socket.js";
+import path from "path";
 
 dotenv.config();
 
@@ -16,25 +16,21 @@ app.use(
     credentials: true,
   })
 );
-app.use(express.json({limit: "10mb"}));
+app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
-
-
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoute);
 
-app.get("/", (req, res) => {
-  res.send("home page");
-});
 const PORT = process.env.PORT || 3000;
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 
-if(process.env.NODE_ENV === "production"){
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
-  })
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
+
 server.listen(PORT, () => {
   console.log(`Server is running or port: ${PORT}`);
   connectDB();
