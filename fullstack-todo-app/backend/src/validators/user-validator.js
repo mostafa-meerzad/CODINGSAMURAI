8b8455acae70d122dfe49ea2ab191654029bd7b8
Mostafa Meerzad <mostafameerzad@gmail.com>
2025-03-23 20:09:00 +0430

    todo-app/backend: define schema validators

---
+++ fullstack-todo-app/backend/src/validators/user-validator.js
@@ -0,0 +1,7 @@
+import Joi from "joi";
+
+export const userSchema = Joi.object({
+  name: Joi.string().min(3).max(55).required(),
+  email: Joi.string().min(6).max(155).required().email(),
+  password: Joi.string().min(6).max(50).required(),
+});
