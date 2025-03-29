b488297ed328964f68bc8dba4365a8dd0c80ecd0
Mostafa Meerzad <mostafameerzad@gmail.com>
2025-03-29 13:53:08 +0430

    todo-app/backend: manage CORS policies

--- fullstack-todo-app/backend/src/server.js
+++ fullstack-todo-app/backend/src/server.js
@@ -4,12 +4,15 @@ import { config } from "dotenv";
 import authRoutes from "./routes/auth-route.js";
 import todoRoutes from "./routes/todo-route.js";
 import { connectDB } from "./utils/connectDB.js";
+import cors from "cors";
 
 config();
 
 const app = express();
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/todo", todoRoutes);
 
