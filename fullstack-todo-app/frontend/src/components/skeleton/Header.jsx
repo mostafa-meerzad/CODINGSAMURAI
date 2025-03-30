b0b6b0b8b21510766676fb2c5537fb066da5c2e8
Mostafa Meerzad <mostafameerzad@gmail.com>
2025-03-30 19:42:51 +0430

    todo-app/frontend: add header element

---
+++ fullstack-todo-app/frontend/src/components/skeleton/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className=" flex items-center justify-between w-full ">
+      <h1 className="text-5xl font-semibold uppercase tracking-wider">todo</h1>{" "}
+      <div className="">login</div>
+    </header>
+  );
+};
+
+export default Header;
