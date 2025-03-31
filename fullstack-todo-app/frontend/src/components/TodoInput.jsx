b4837095dd56b7eeffa2db24d7b071acdc2da2a3
Mostafa Meerzad <mostafameerzad@gmail.com>
2025-03-31 22:24:59 +0430

    todo-app/frontend: send a todo to the backend

--- fullstack-todo-app/frontend/src/components/TodoInput.jsx
+++ fullstack-todo-app/frontend/src/components/TodoInput.jsx
@@ -1,27 +1,30 @@
 import React from "react";
-
 import { useForm } from "react-hook-form";
 import check from "../assets/icon-check.svg";
+import { axiosInstance } from "../axios";
+import toast from "react-hot-toast";
 
 const TodoInput = () => {
   const { register, handleSubmit } = useForm();
 
-  const onSubmit = (data) => {
-    console.log("form data: ", data);
+  const onSubmit = async (data) => {
+    try {
+      console.log("form data: ", data);
+      const response = await axiosInstance.post("/todo", data)
+      console.log("response from adding a todo: ", response)
+    } catch (error) {
+      console.log("something went wrong, ", error)
+      toast.error("can't add Todo!")
+    }
   };
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
       className="flex items-center justify-between md:gap-5 px-5 py-4 rounded-md mt-12 mb-8 bg-very-dark-grayish-blue "
     >
-      <label className="relative flex items-center cursor-pointer">
-        <input type="checkbox" {...register("isCompleted")} className="peer hidden" />
-        <div className="w-6 h-6 rounded-full border-2 border-gray-400 bg-white flex items-center justify-center transition-all duration-300 peer-checked:border-transparent peer-checked:bg-radial-[at_50%_75%] from-check-background-from  to-check-background-to to-90%  ">
-          <img src={check} alt="check" className="" />
-        </div>
-      </label>
+      
 
-      <label className="w-2/3">
+      <label className="w-full">
         <input
           type="text"
           {...register("task", { required: true })}
@@ -30,7 +33,9 @@ const TodoInput = () => {
         />
       </label>
 
-      <button type="submit" className="capitalize hover:text-indigo-400 p-3">add</button>
+      <button type="submit" className="capitalize hover:text-indigo-400 p-3">
+        add
+      </button>
     </form>
   );
 };
