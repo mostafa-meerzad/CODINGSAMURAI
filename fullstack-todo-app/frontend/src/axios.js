7e335aa8cbdf06026f9739a933cdd3b449f2a775
Mostafa Meerzad <mostafameerzad@gmail.com>
2025-04-01 22:09:56 +0430

    todo-app/frontend: update axios base url

--- fullstack-todo-app/frontend/src/axios.js
+++ fullstack-todo-app/frontend/src/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-const baseUrl = import.meta.env.VITE_BASE_URL;
 
 export const axiosInstance = axios.create({
-  baseURL: baseUrl,
+  baseURL:  import.meta.env.MODE === "development" ? "http://localhost:3000/api": "/api",
   withCredentials: true,
 });
